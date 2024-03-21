ecdb7bcf164c807ac20bb570d343cbf4a07bffc9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 08:47:27 +0430

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
   const [loading, setLoading] = useState(true)
   const [person, setPerson] = useState(null)
+  const [title, setTitle] = useState('name')
+  const [title, setTitle] = useState('name')
+  
   
   return <h2>random user starter</h2>
 }
