9ed6c93c4879dbff79ad8e68aea6d67a7d1f21c8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 09:14:14 +0430

    init

--- src/App.js
+++ src/App.js
@@ -28,7 +28,9 @@ function App() {
         <p className="user-title">my {title} is</p>
         <p className='user-value'>{value}</p>
         <div className="values-list">
-          <button className='icon' data-label='name' onMouseOver={handleValue}></button>
+          <button className='icon' data-label='name' onMouseOver={handleValue}>
+            <FaUser />
+          </button>
         </div>
       </div>
     </div>
