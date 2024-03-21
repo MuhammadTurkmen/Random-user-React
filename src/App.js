8b745d0b449235194f527a3aa75386bc6e9d86c6
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 09:19:57 +0430

    init

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           <button className='icon' data-label='email' onMouseOver={handleValue}><FaEnvelopeOpen /></button>
           <button className='icon' data-label='age' onMouseOver={handleValue}><FaCalendarTimes /></button>
           <button className='icon' data-label='street' onMouseOver={handleValue}><FaMap /></button>
-          <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
+          <button className='icon' data-label='phone' onMouseOver={handleValue}><FaPhone /></button>
           <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
         </div>
       </div>
