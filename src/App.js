57d26244347127ac32cc230f47625a5a24d38b3d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 09:17:40 +0430

    init

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <div className="values-list">
           <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
           <button className='icon' data-label='email' onMouseOver={handleValue}><FaEnvelopeOpen /></button>
-          <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
+          <button className='icon' data-label='age' onMouseOver={handleValue}><FaCalendarTimes /></button>
           <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
           <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
           <button className='icon' data-label='name' onMouseOver={handleValue}><FaUser /></button>
