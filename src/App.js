08fc58f1f99bf93eea8da33317c9a8c781cc0f66
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 10:22:39 +0430

    init

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ function App() {
   const handleValue = (e) => {
     if(e.target.classList.contains()) {
       const newValue = e.target.dataset.label
+      setTitle(newValue)
       setValue(person[newValue])
     }
   }
